4313dcc06301ac7f07010b183260ff910bd3e657
Vitória <jeanevitoria994@gmail.com>
2024-10-31 18:48:02 -0300

    feat: service de integração das páginas de login e cadas tro.

---
+++ src/services/userService.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export const cadastroService = async (data) => {
+    return axios.post('https://adotepet-api.vercel.app/api/auth/cadastro', data, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then ((result) => { return result })
+    .catch((err) => { throw new Error(err.message) })
+}
+
+export const loginService = async (data) => {
+    return axios.post('https://adotepet-api.vercel.app/api/auth/login', data, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then((result) => { return result })
+    .catch((err) => { throw new Error(err.message) })
+}
